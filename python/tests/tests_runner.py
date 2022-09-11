d1cbcc452cb90b51160d90354d29e4a9f248a318
Christian Pojoni <dev@pojoni.at>
2022-09-11 21:38:58 +0200

    print to stderr

--- python/tests/tests_runner.py
+++ python/tests/tests_runner.py
@@ -14,7 +14,7 @@ def init() -> list:
     for file in files:
         with open(file, "r") as stream:
             contents = yaml.safe_load(stream)
-            tests.extend(list(map(lambda x: [f"{file:30s} | {x['name']}", x['input'], x['output']], contents)))
+            tests.extend(list(map(lambda x: [f"{file:25s} | {x['name']}", x['input'], x['output']], contents)))
     return tests
 
 
@@ -26,13 +26,13 @@ class RunnerTests(unittest.TestCase):
 
     @parameterized.expand(tests)
     def test_run(self, name, inp, expected):
-        print('❌', name, end='')
+        print('❌', name, end='', file=sys.stderr)
         try:
             output = self.parser.parse(inp)
             self.assertEqual(expected, output, msg=f'Error in {name}')
-            print('\r✅', name)
+            print('\r✅', name, file=sys.stderr)
         except Exception as ex:
-            print()
+            print(file=sys.stderr)
             raise ex
 
 
