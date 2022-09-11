47f1347757fdee0469712e6722e741b8474bd4f7
Christian Pojoni <dev@pojoni.at>
2022-09-11 12:35:02 +0200

    replacing old test with test runner

---
+++ python/tests/tests_runner.py
@@ -0,0 +1,27 @@
+import unittest
+import yaml
+import glob
+
+from parameterized import parameterized
+
+from src.parser import Parser
+
+tests = []
+for file in glob.glob('*.yaml'):
+    with open(file, "r") as stream:
+        try:
+            contents = yaml.safe_load(stream)
+            tests.extend(list(map(lambda x: [x['name'], x['input'], x['output']], contents)))
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+class RunnerTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.parser = Parser()
+
+    @parameterized.expand(tests)
+    def test_run(self, name, inp, expected):
+        output = self.parser.parse(inp)
+        self.assertEqual(output, expected, msg=f'Error in {name}')
