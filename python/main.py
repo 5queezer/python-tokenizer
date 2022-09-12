eb65354106ed8b55996941c524a87b0eb91f8f71
Christian Pojoni <dev@pojoni.at>
2022-09-12 21:43:11 +0200

    main executable

--- python/main.py
+++ python/main.py
@@ -1,26 +1,37 @@
 import yaml
 import json
+import sys
+from argparse import ArgumentParser, Namespace
 from src.parser import Parser
 
-if __name__ == '__main__':
+def arguments() -> Namespace:
+    p = ArgumentParser(description='Parse letter files.')
+    p.add_argument('-e', '--expression', help='parse expression')
+    p.add_argument('-f', '--file', help='parse file')
+    p.add_argument('--format', help='output format', default='yaml', choices='yaml, json')
+    args = p.parse_args()
+    return args
+
+def dumper(format, ast):
+    if format == 'yaml':
+        return yaml.dump(ast, sort_keys=False)
+    else:
+        return json.dumps(ast, indent=2, sort_keys=False)
+def main():
+    args = arguments()
+    if args.expression:
+        expression = args.expression
+    elif args.file:
+        with open(args.file) as file:
+            expression = file.read()
+    else:
+        expression = sys.stdin.read()
+
     parser = Parser()
-    input = '''
-    let s = "Hello, world";
-    let i = 0;
-    
-    while (i < s.length) {
-        s[i];
-        // console.log(i, s[i]);
-        i += 1;
-    }
+    ast = parser.parse(expression)
+    out = dumper(args.format, ast)
+    print(out)
 
-    '''
-    output = parser.parse(input)
-    test = {
-        'input': input,
-        'output': output
-    }
-    print(input)
-    print(yaml.dump(output, sort_keys=False))
-    # print(json.dumps(test, indent=2))
 
+if __name__ == '__main__':
+    main()
